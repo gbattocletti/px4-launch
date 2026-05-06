d7213053ea9dad08052a39e4b5ae3d51ed8e4346
Gianpietro Battocletti <battocletti.gianpietro@gmail.com>
2026-05-06 19:04:40 +0200

    Fix tmux bug

--- px4_sitl_launcher.py
+++ px4_sitl_launcher.py
@@ -215,6 +215,7 @@ def _launch_tmux(commands, session, delay):
             [
                 "tmux",
                 "new-window",
+                "-a",
                 "-t",
                 session,
                 "-n",
